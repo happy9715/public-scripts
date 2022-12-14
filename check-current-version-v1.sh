97e2ba48a555f9b0abcf6f8096fcff1942c17218
Denis Larkin <deniswork@techjedi.co>
2022-12-14 11:45:48 +0300

    add version sh

---
+++ check-current-version-v1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# v1.0.0
+
+# INPUT VIARBALES
+APPNAME=$1
+CURRENT_VER=$2
+CH_ADDRESS=$3
+CH_LOGIN=$4
+CH_PASS=$5
+HTTP_HEADER_NAME=$6
+HTTP_HEADER_VALUE=$7
+
+
+if [ -z "$CI_COMMIT_TAG" ]
+then
+    CURRENT_VER=$2
+    echo "tag not set current version = $CURRENT_VER"
+else
+    CURRENT_VER=$CI_COMMIT_TAG
+    echo "tag SET current version = $CURRENT_VER"
+fi
+
+# debug string
+echo "appname - $APPNAME, current_ver = $CURRENT_VER, ch_addr = $CH_ADDRESS, ch_login = $CH_LOGIN, http_header = $HTTP_HEADER_NAME"
+
+VERSION_RESULT=$(echo "SELECT version FROM cicd.appversion FINAL WHERE appname = '$APPNAME'  and version ='$CURRENT_VER'" | curl --silen -H "$HTTP_HEADER_NAME:$HTTP_HEADER_VALUE" -X POST  https://$CH_LOGIN:$CH_PASS@$CH_ADDRESS --data-binary "@-")
+echo "check old version result: old version = $VERSION_RESULT, current version = $CURRENT_VER"
+
